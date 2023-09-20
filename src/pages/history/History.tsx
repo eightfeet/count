ed62dc67e2a20c4dc27941ad44abf9f99dfd7da3
root <root@GFGZ-xiaojn.by-healthdc.com>
2023-09-20 14:21:52 +0800

    update

--- src/pages/history/History.tsx
+++ src/pages/history/History.tsx
@@ -1,16 +1,33 @@
-import React from 'react';
-// import s from './History.module.scss';
+import { Button, Space } from "antd-mobile";
+import React from "react";
+import s from "./History.module.scss";
+import { useSnapshot } from "valtio";
+import { runningTime } from "~/store";
 
-interface Props {
-    
-}
+interface Props {}
 
-const History:React.FC<Props> = () => {
-    return (
-        <div>
-            asdasd
+const History: React.FC<Props> = () => {
+  const { history } = useSnapshot(runningTime);
+  return (
+    <>
+      {history?.map((item, index) => (
+        <div className={s.page} key={index}>
+          <Space block wrap>
+            {item.subject?.map((el, index) => (
+              <div className={s.item}>
+                <span>（{index + 1}）</span> {el.equationStr}
+                <span>&nbsp;{el.result}</span>
+              </div>
+            ))}
+          </Space>
+          <footer className={s.footer}>{item.startAt}</footer>
         </div>
-    )
-}
+      ))}
+      <Button className={s.printbutton} onClick={() => window.print()}>
+        打印
+      </Button>
+    </>
+  );
+};
 
 export default History;
