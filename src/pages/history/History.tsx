78ef0c4dc3fe4280e44b810d85554e4ea1fef484
root <root@GFGZ-xiaojn.by-healthdc.com>
2023-09-21 09:55:27 +0800

    update

--- src/pages/history/History.tsx
+++ src/pages/history/History.tsx
@@ -1,32 +1,64 @@
-import { Button, Space } from "antd-mobile";
+import { Button, Divider, Space } from "antd-mobile";
 import React from "react";
 import s from "./History.module.scss";
 import { useSnapshot } from "valtio";
 import { runningTime } from "~/store";
+import { useNavigate } from "react-router-dom";
 
 interface Props {}
 
 const History: React.FC<Props> = () => {
   const { history } = useSnapshot(runningTime);
+  const navigator = useNavigate();
+
   return (
-    <>
+    <div>
+      <br />
+      <Space block justify="center">
+        <Button block className={s.printbutton} onClick={() => navigator("/")}>
+          出题
+        </Button>
+        <Button block className={s.printbutton} onClick={() => window.print()}>
+          打印
+        </Button>
+        <Button
+          block
+          className={s.printbutton}
+          onClick={() => (runningTime.history = [])}
+        >
+          清空记录
+        </Button>
+      </Space>
+      <br />
+
       {history?.map((item, index) => (
         <div className={s.page} key={index}>
-          <Space block wrap>
-            {item.subject?.map((el, index) => (
-              <div className={s.item}>
-                <span>（{index + 1}）</span> {el.equationStr}
-                <span>&nbsp;{el.result}</span>
-              </div>
-            ))}
-          </Space>
-          <footer className={s.footer}>{item.startAt}</footer>
+          <div>
+            <Space block wrap>
+              {item.subject?.map((el, index) => (
+                <div className={s.item}>
+                  <span className={s.index}>{index + 1}</span> {el.equationStr}
+                  <span>&nbsp;</span>
+                </div>
+              ))}
+            </Space>
+          </div>
+          <div className={s.result}>
+            <Divider>答案</Divider>
+            <Space block wrap>
+              {item.subject?.map((el, index) => (
+                <div className={s.item}>
+                  <span className={s.index}>{index + 1}</span> {el.equationStr}
+                  <span style={{ fontWeight: "bolder" }}>
+                    &nbsp;{el.result}
+                  </span>
+                </div>
+              ))}
+            </Space>
+          </div>
         </div>
       ))}
-      <Button className={s.printbutton} onClick={() => window.print()}>
-        打印
-      </Button>
-    </>
+    </div>
   );
 };
 
